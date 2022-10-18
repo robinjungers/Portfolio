67449c376f96ded6118c54e9e994fe3efde642c0
Robin Jungers <robin@jungers.fr>
2022-10-18 18:31:36 +0200

    Added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,7 @@ export default function _App( { Component, pageProps } ) {
 			<Head>
 				<title>Robin Jungers - Creative direction, interactive software</title>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<link rel="icon" href="data:," />
+				<link rel="icon" type="image/png" href="/images/favicon.png" />
 			</Head>
 			
 			<Component { ...pageProps } />
