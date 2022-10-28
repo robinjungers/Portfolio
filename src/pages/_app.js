6c0bfff9a8fcd9f5f98179fbbb7355f4fa4c00de
Robin Jungers <robin@jungers.fr>
2022-10-28 19:06:01 +0200

    Added Plausible script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,14 +9,15 @@ import Script from 'next/script';
 
 export default function _App( { Component, pageProps } ) {
 	return (
-		<div>
+		<>
 			<Head>
 				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 				<link rel="icon" type="image/png" href="/images/favicon.png" />
-				<Script data-domain="robinjungers.com" src="https://plausible.io/js/plausible.js" />
 			</Head>
 			
 			<Component { ...pageProps } />
-		</div>
+
+			<Script data-domain="robinjungers.com" src="https://plausible.io/js/plausible.js" />
+		</>
 	);
 }
